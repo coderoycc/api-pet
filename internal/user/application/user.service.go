aa249c7e367afbeae96d21744667c82378a9d6f6
coderoycc <rcchambi4@gmail.com>
2026-07-18 17:41:30 -0400

    refact: userService and userDomain

--- internal/user/application/user.service.go
+++ internal/user/application/user.service.go
@@ -21,6 +21,19 @@ func NewUserService(repo domain.UserRepository) *UserService {
 	}
 }
 
+func (s *UserService) Get(ctx context.Context, id string) (*UserResponse, error) {
+	user, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &UserResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt.Format("Y-m-d"),
+	}, nil
+}
+
 func (s *UserService) Create(ctx context.Context, req CreateUserRequest) (*UserResponse, error) {
 	if err := s.validate.Struct(req); err != nil {
 		return nil, err
@@ -43,4 +56,35 @@ func (s *UserService) Create(ctx context.Context, req CreateUserRequest) (*UserR
 		Name:  user.Name,
 		Email: user.Email,
 	}, nil
+}
+
+func (s *UserService) Update(ctx context.Context, id string, req UpdateUserRequest) (*UserResponse, error) {
+	user, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.Name != nil {
+		user.Name = *req.Name
+	}
+	if req.Email != nil {
+		user.Email = *req.Email
+	}
+	if err := s.repo.Update(ctx, user); err != nil {
+		return nil, err
+	}
+	return &UserResponse{
+		ID:    user.ID,
+		Email: user.Email,
+		Name:  user.Name,
+	}, nil
+}
+
+func (s *UserService) Delete(ctx context.Context, id string, softOpt ...bool) error {
+	soft := true
+	if len(softOpt) > 0 {
+		soft = softOpt[0]
+	}
+
+	return s.repo.Delete(ctx, id, soft)
 }
--- internal/user/domain/user.go
+++ internal/user/domain/user.go
@@ -10,6 +10,7 @@ type User struct {
 	Name      string
 	Email     string
 	Password  string
+	deleted   bool
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -19,5 +20,5 @@ type UserRepository interface {
 	FindByID(ctx context.Context, id string) (*User, error)
 	FindByEmail(ctx context.Context, email string) (*User, error)
 	Update(ctx context.Context, user *User) error
-	Delete(ctx context.Context, id string) error
+	Delete(ctx context.Context, id string, args ...bool) error
 }
