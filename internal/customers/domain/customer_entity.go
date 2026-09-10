98c1a13f72a5681c04a9693e493bd861fca6b8a2
rchambi <rchambi@hansa.com.bo>
2026-09-10 16:55:09 -0400

    feat: customer changes handler and entity.

--- internal/customers/domain/customer_entity.go
+++ internal/customers/domain/customer_entity.go
@@ -7,20 +7,20 @@ import (
 )
 
 type Customer struct {
-	ID             uuid.UUID
-	Name           string
-	DocumentType   string
-	DocumentNumber string
-	Email          *string
-	Phone          *string
-	MobilePhone    *string
-	Address        *string
-	City           *string
-	Department     *string
-	BusinessName   *string
-	TaxCategory    *string
-	Notes          *string
-	Status         string // active, inactive, suspended
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	ID             uuid.UUID `json:"id"`
+	Name           string    `json:"name"`
+	DocumentType   string    `json:"documentType"`
+	DocumentNumber string    `json:"documentNumber"`
+	Email          *string   `json:"email"`
+	Phone          *string   `json:"phone"`
+	MobilePhone    *string   `json:"mobilePhone"`
+	Address        *string   `json:"address"`
+	City           *string   `json:"city"`
+	Department     *string   `json:"department"`
+	BusinessName   *string   `json:"businessName"`
+	TaxCategory    *string   `json:"taxCategory"`
+	Notes          *string   `json:"notes"`
+	Status         string    `json:"status"` // active, inactive, suspended
+	CreatedAt      time.Time `json:"createdAt"`
+	UpdatedAt      time.Time `json:"updatedAt"`
 }
